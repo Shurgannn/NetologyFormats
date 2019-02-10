cc528632cf6e5a0f55d20f5577055dcf8957bf5f
Shurgannn <46814630+Shurgannn@users.noreply.github.com>
2019-02-10 22:38:48 +0300

    Add files via upload

--- hw_work_with_formats.py
+++ hw_work_with_formats.py
@@ -2,37 +2,47 @@ from pprint import pprint
 import json
 
 def get_words_in_description():
+    list_of_des = []
     for f in item:
         for k, values in f.items():
             if k == 'description':
                 words_in_description = values.split()
-                print(words_in_description)
-    return words_in_description
+                list_of_des.append(words_in_description)
+    return list_of_des
 
 def top10():
-    for word in words_in_description:
-        if len(word) > 6:
-            word6 = word.strip()
-        if word6 in words_dict:
-            words_dict[word6] += 1
-        else:
-            words_dict[word6] = 0
-        print(words_dict)
-        words_dict = sorted(words_dict.items())
-        print(words_dict)
+    words_dict = {}
+    for w in get_words_in_description():
+        for word in w:
+            #pprint(word)
+            if len(word) > 6:
+                word6 = word
+                if word6 in words_dict:
+                    words_dict[word6] += 1
+                else:
+                    words_dict[word6] = 0
+    list = []
+    for i in words_dict:
+        k = (i, words_dict[i])
+        list.append(k)
+    sorlist = sorted(list, key=lambda s: s[1], reverse=True)
+    sorlist = sorlist[0:10]
+    print('топ 10 самых часто встречающихся в новостях слов длиннее 6 символов:')
+    for i in sorlist:
+        print(f'слово "{i[0]}" - {i[1]} раз')
     return words_dict
 
 with open('newsafr.json', encoding='utf8') as datafile:
     json_data = json.load(datafile)
     item = json_data['rss']['channel']['items']
-    #get_words_in_description()
-    #words_dict = {}
-    #top10()
+    top10()
 
 
 import xml.etree.ElementTree as ET
 tree = ET.parse("newsafr.xml")
 root = tree.getroot()
+xml_description = root.findall("channel/item/description")
+
 #print(root.tag)
 #print(root.attrib)
 #xml_title = root.find("channel/title")
@@ -42,8 +52,35 @@ root = tree.getroot()
 #for xmli in xml_items:
     #print(xmli.attrib["id"])
 
+def get_words_in_description_xml():
+    list_of_des = []
+    for description in xml_description:
+        list = [description.text]
+        for l in list:
+            words_in_description = l.split()
+            list_of_des.append(words_in_description)
+    return list_of_des
 
-xml_description = root.findall("description/description")
-print(xml_description)
-for description in xml_description:
-    print(description.attrib)
+def top10_xml():
+    words_dict = {}
+    for w in get_words_in_description_xml():
+        for word in w:
+            #pprint(word)
+            if len(word) > 6:
+                word6 = word
+                if word6 in words_dict:
+                    words_dict[word6] += 1
+                else:
+                    words_dict[word6] = 0
+    list = []
+    for i in words_dict:
+        k = (i, words_dict[i])
+        list.append(k)
+    sorlist = sorted(list, key=lambda s: s[1], reverse=True)
+    sorlist = sorlist[0:10]
+    print('топ 10 самых часто встречающихся в новостях слов длиннее 6 символов:')
+    for i in sorlist:
+        print(f'слово "{i[0]}" - {i[1]} раз')
+    return words_dict
+
+top10_xml()
